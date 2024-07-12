ea41aa4c227c5e0c518f7ed8ee2b98a2e12f53d6
Alexis-Srrn <alexis_srrn96@outlook.com>
2024-07-11 19:37:03 -0600

    maquetación del sitio pagina inicial al 50%

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+/** Para desblegar el navbar en el responsive*/
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+
+/** Swiper  */
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
