091ff48f7679db649f4bdb1167dc69cf80ec0064
Alexis-Srrn <alexis_srrn96@outlook.com>
2024-07-15 17:22:32 -0600

    página principal del hotel creada

--- public/js/script.js
+++ public/js/script.js
@@ -10,11 +10,20 @@ window.onscroll = () => {
 }
 
 
+/**Preguntas frecuentes */
+document.querySelectorAll('.contact .row .faq .box').forEach(faqBox =>{
+    faqBox.onclick = () =>{
+        faqBox.classList.toggle('active');
+    }
+});
+
+
 /** Swiper  */
 var swiper = new Swiper(".home-slider", {
     loop: true,
     effect: "coverflow",
     grabCursor: true,
+    spaceBetween: 30,
     coverflowEffect: {
         rotate: 50,
         stretch: 0,
@@ -26,4 +35,42 @@ var swiper = new Swiper(".home-slider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+
+var swiper = new Swiper(".gallery-slider", {
+    loop: true,
+    effect: "coverflow",
+    centeredSlides: true,
+    slidesPerView: "auto",
+    grabCursor: true,
+    coverflowEffect: {
+        rotate: -0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+});
+
+
+var swiper = new Swiper(".reviews-slider", {
+    loop: true,
+    slidesPerView: "auto",
+    grabCursor: true,
+    spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+        },
+        1200: {
+          slidesPerView: 2,
+        },
+      },
 });
